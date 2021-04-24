8e432452bbfd22c888978fa2a6c752843542bcb8
Stephen Barbour <SBARBOUR@bethel.jw.org>
2021-04-23 23:37:57 -0400

    Initial Commit to building a TekChatServer

---
+++ TekChatServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TekChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
